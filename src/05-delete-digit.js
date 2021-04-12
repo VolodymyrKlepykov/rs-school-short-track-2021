24fd6035556e27487f32df4031c86f08f43c0091
VolodymyrKlepykov <volodymyr.klepykov@gmail.com>
2021-04-12 11:00:52 +0300

    feat: completed tast 5

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,13 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const arr = n
+    .toString()
+    .split('')
+    .map((el) => Number(el));
+  const min = Math.min(...arr);
+  return arr.splice(arr.indexOf(min), 1) ? Number(arr.join('')) : 0;
 }
 
 module.exports = deleteDigit;

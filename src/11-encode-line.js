1d01fe9002e212f44b8da680bd6cc296c469b614
VolodymyrKlepykov <volodymyr.klepykov@gmail.com>
2021-04-12 11:26:07 +0300

    feat: completed task 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,18 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let line = '';
+  let count = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      count++;
+    } else {
+      line += `${count}${str[i]}`;
+      count = 1;
+    }
+  }
+  return line.replace(/1/g, '');
 }
 
 module.exports = encodeLine;
